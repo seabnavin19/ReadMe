9b06afa155ab313205eb5a5870604d7d6c6a8210
vannakml <>
2021-05-24 06:08:12 +0700

    TableList.vue coustomized

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -1,47 +1,47 @@
 <template>
  
   <form>
+  <div class="form-group">
+      <label>Description</label>
+      <textarea name="description" id="description" rows="3"></textarea>
+    </div>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      <label for="inputEmail4">Age</label>
+      <input type="number" class="form-control" id="inputEmail4" placeholder="Age" max="100" min="18">
     </div>
     <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
+      <label for="inputState">Group</label>
       <select id="inputState" class="form-control">
         <option selected>Choose...</option>
-        <option>...</option>
+        <option>Group 1</option>
+        <option>Group 2</option>
+        <option>Group 3</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputState">Services</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>Service 1</option>
+        <option>Service 2</option>
+        <option>Service 3</option>
       </select>
     </div>
     <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
+      <label for="inputState">Gender</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>Male</option>
+        <option>Female</option>
+        <option>Other</option>
+      </select>
     </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
+    <div class="form-group col-md-4">
+      <label for="inputCity">Fee</label>
+      <input type="number" class="form-control" id="inputFee" placeholder="$" >
     </div>
   </div>
   <button type="submit" class="btn btn-primary">Sign in</button>
@@ -61,4 +61,13 @@ form{
 table{
   width: 100%;
 }
+textarea{
+  width: 100%;
+  border: medium none;
+  border-radius: 4px;
+  font-size: 14px;
+  padding: 7px 18px;
+  background-color: #fffcf5;
+}
+
 </style>
