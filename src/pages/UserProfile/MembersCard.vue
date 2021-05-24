bd809e0a52f7e842b43b844e537fefecd7d0da6b
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2021-05-24 22:25:46 +0700

    new change calculator

--- src/pages/UserProfile/MembersCard.vue
+++ src/pages/UserProfile/MembersCard.vue
@@ -1,142 +1,78 @@
 <template>
-    <!-- <card class="card" title="Customer's Profile">
-        <div>
-          <form @submit.prevent>
-            <div class="row">
-              <div class="col-md-6">
-                <fg-input type="text"
-                          label="Username"
-                          placeholder="Username"
-                          v-model="user.username">
-                </fg-input>
-              </div>
-              <div class="col-md-6">
-                <fg-input type="text"
-                          label="Account Number"
-                          placeholder="Account Number"
-                          v-model="user.accountNumber">
-                </fg-input>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <fg-input type="text"
-                          label="Cards obtained"
-                          placeholder="Number of cards"
-                          v-model='user.has_credit_card'>
-                </fg-input>
-              </div>
-              <div class="col-md-6">
-                <fg-input type="text"
-                          label="Year with Bank"
-                          placeholder="Tenure"
-                          v-model="user.tenure">
-                </fg-input>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-4">
-                <fg-input type="text"
-                          label="Balance"
-                          placeholder="Balance"
-                          v-model="user.balance">
-                </fg-input>
-              </div>
-                        <div class="col-md-4">
-                <fg-input type="text"
-                          label="Group"
-                          placeholder="Group"
-                          v-model="user.group">
-                </fg-input>
-              </div>
-                        <div class="col-md-4">
-                <fg-input type="text"
-                          label="Number of Product"
-                          placeholder="Number of product"
-                          v-model="user.numberOfProduct">
-                </fg-input>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <div class="form-group">
-                  <label> Customer's Special Offer</label>
-                  <textarea rows="5" class="form-control border-input"
-                            placeholder="Special Offer"
-                            v-model="user.specialOffer">
-                  </textarea>
-                </div>
-              </div>
-            </div>
-            <div class="text-center">
-              <p-button type="info"
-                        round
-                        @click.native.prevent="updateProfile">
-                Update Profile
-              </p-button>
-            </div>
-            <div class="clearfix"></div>
-          </form>
-        </div>
-    </card> -->
+
   <card class="card-user" title="Confession Fee">
-      
+
     <div class="form-row">
+
+
       <div class="form-group col-md-6">
+ 
         <label for="number">Money of Transaction</label>
         <input type="number" class="form-control" placeholder="Money of Transaction" v-model="moneyOfTransaction" >
+ 
       </div>
+
+
       <div class="form-group col-md-6">
+
         <label for="inputState">Service</label>
+
         <select id="selectedService" class="form-control" v-model="selectedService">
+
           <option selected  >Choose Service</option>
           <option>OTT</option>
           <option>ITT</option>
           <option>Water Bill</option>
           <option>EDC Bill</option>
           <option>Other Services</option>
+
         </select>
+
       </div>
-      
+
+
     </div>
+
     <div class="form-row">
+
       <div class="form-group col-md 6">
+
+        <label>Minimum Fee</label>
+        <input type="number" class="form-control" placeholder="Minimum Fee" v-model="minFee">
+
+      </div>
+      <div class="form-group col-md 6">
+
+        <label>Maximum Fee</label>
+        <input type="number" class="form-control" placeholder="Maximum Fee" v-model="maxFee">
+
+      </div>
+    
+    </div>
+
+    <div class="form-row">
+
+      <div class="form-group col-md 6">
+
         <label>Calculated Confession Fee</label>
         <input type="number" class="form-control" placeholder="Confession Fee" v-model="calculatedFee">
-      </div>
-      
-      
-    </div>
-    <div class="row">
-        <button type="submit" class="btn btn-primary" @click="getStatusClass(selectedService)">submit</button>
-      </div>
-   
-    <!-- <div>
-      <ul class="list-unstyled team-members">
-        <li>
-          <div class="row" v-for="member in members" :key="member.name">
-            <div class="col-3">
-              <div class="avatar">
-                <img :src="member.image" alt="Circle Image" class="rounded img-fluid">
-              </div>
-            </div>
-            <div class="col-6">
-              {{member.name}}
-              <br>
-              <span :class="getStatusClass(member.status)">
-                <small>{{member.status}}</small>
-              </span>
-            </div>
 
-            <div class="col-3">
-              <p-button type="success" outline icon>
-                <i class="fa fa-envelope"></i>
-              </p-button>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div> -->
+      </div>
+
+      <div class="form-group col-md 6">
+
+        <label>Commission Fee</label>
+        <input type="number" class="form-control" placeholder="Commission Fee" v-model="comFee">
+
+      </div>
+    
+    </div>
+
+    <div class="row">
+        <div class="col-5"></div>
+        <button type="submit" class="btn btn-primary " @click="getStatusClass(selectedService)">submit</button>
+    </div>
+
   </card>
   
 </template>
@@ -147,7 +83,9 @@ export default {
 
   data() {
     return {
-     
+        minFee:null,
+        maxFee:null,
+        comFee:null,
         moneyOfTransaction :null,
 
         selectedService: null,
@@ -192,19 +130,9 @@ export default {
                 this.ITT.maxFee=customer1.Max_Fee
                 this.ITT.comFee=customer1.comision
 
-                
-                
-
-                
-                // console.log
               }
-
-
             }
             
-
-            
-      
           })
         })
      
@@ -221,23 +149,30 @@ export default {
       return this.calculatedFee
       
     },
-    me(){
-      console.log(this.selectedService)
-      return this.selectedService
-      
-    },
     
     getStatusClass(selectedService) {
       
       switch (selectedService) {
         case "OTT":
+          this.minFee = this.OTT.minFee
+          this.maxFee = this.OTT.maxFee
+          this.comFee = this.OTT.comFee
           return this.getFee(this.moneyOfTransaction,this.OTT.comFee/100,this.OTT.minFee,this.OTT.maxFee);
         case "ITT":
-          return this.getFee(this.user,moneyOfTransaction);
+          this.minFee = this.ITT.minFee
+          this.maxFee = this.ITT.maxFee
+          this.comFee = this.ITT.comFee
+          return this.getFee(this.moneyOfTransaction, this.ITT.comFee/100, this.ITT.minFee, this.ITT.maxFee);
         case "Water Bill":
-          return this.getFee(this.user.moneyOfTransaction);
+          this.minFee = 0
+          this.maxFee = 0
+          this.comFee = 0
+          return this.getFee(this.moneyOfTransaction, 0, 0, 0);
         case "EDC Bill":
-          return this.getFee(this.user.moneyOfTransaction);
+          this.minFee = 0
+          this.maxFee = 0
+          this.comFee = 0
+          return this.getFee(this.moneyOfTransaction, 0, 0, 0);
         default:
           return "text-success";
       }
